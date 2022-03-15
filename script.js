12ad222fd28008ea4437b793c8fc982548e01140
pablofsc <pablofsc@outlook.com>
2022-03-15 17:08:26 -0300

    Refatora várias funções

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-var tabuleiro = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '];
-var tabaux = [];
+var tabuleiroReal = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '];
 
 var botoes = [];
 
@@ -16,6 +15,25 @@ window.onload = function () {
         botoes[i].addEventListener("click", () => checarValidez(i));
     }
 
+    atualizarPlacares();
+}
+
+function novaPartida() {
+    for (i = 0; i <= 8; i++) { // limpa o tabuleiro
+        tabuleiroReal[i] = ' ';
+    }
+
+    primeiroJogador = trocarJogador(primeiroJogador)
+    jogadorAtual = primeiroJogador;
+
+    atualizarBotoes();
+
+    if (primeiroJogador === 'o') {
+        iaIniciar('o');
+    }
+}
+
+function atualizarPlacares() {
     document.getElementById("partidasVencidasJogador").innerHTML = partidasVencidasX;
     document.getElementById("partidasVencidasJogadorH1").innerHTML = partidasVencidasX;
     document.getElementById("partidasVencidasMaquina").innerHTML = partidasVencidasO;
@@ -25,7 +43,7 @@ window.onload = function () {
 
 function checarValidez(casa) {
     /*console.log("Checando validez de " + casa);*/
-    if (tabuleiro[casa] !== ' ') {
+    if (tabuleiroReal[casa] !== ' ') {
         console.log("Seleção inválida: " + casa + " por " + jogadorAtual);
         return;
     }
@@ -35,7 +53,7 @@ function checarValidez(casa) {
 
 function registrarJogada(casa) {
     console.log("Registrando: " + casa + " por " + jogadorAtual);
-    tabuleiro[casa] = jogadorAtual;
+    tabuleiroReal[casa] = jogadorAtual;
 
     atualizarBotoes();
 
@@ -44,44 +62,33 @@ function registrarJogada(casa) {
     jogadorAtual = trocarJogador(jogadorAtual);
     console.log("Próximo jogador: " + jogadorAtual);
 
-    if (jogadorAtual === 'o' && checarVencedorHipotetico(tabuleiro) == ' ') setTimeout(() => { iaIniciar('o'); }, 100);
+    if (jogadorAtual === 'o' && checarVencedorHipotetico(tabuleiroReal) == ' ') setTimeout(() => { iaIniciar('o'); }, 100);
 }
 
 function checarVencedorReal() {
-    vencedor = checarVencedorHipotetico(tabuleiro)
+    vencedor = checarVencedorHipotetico(tabuleiroReal)
+
     if (vencedor !== ' ') {
         console.log("---------------- Anunciando vencedor: " + vencedor);
 
-        if (vencedor == 'x') {
-            partidasVencidasX++;
-            document.getElementById("partidasVencidasJogador").innerHTML = partidasVencidasX;
-            document.getElementById("partidasVencidasJogadorH1").innerHTML = partidasVencidasX;
-            sleep(750);
-        }
-        else if (vencedor == '!') {
-            empates++
-            document.getElementById("partidasEmpatadas").innerHTML = empates;
-            sleep(750);
-        }
-        else {
-            partidasVencidasO++;
-            document.getElementById("partidasVencidasMaquina").innerHTML = partidasVencidasO;
-            document.getElementById("partidasVencidasMaquinaH1").innerHTML = partidasVencidasO;
-            sleep(750);
+        switch (vencedor) {
+            case 'x':
+                partidasVencidasX++;
+                break;
+
+            case 'o':
+                partidasVencidasO++;
+                break;
+
+            case '!':
+                empates++;
+                break;
         }
 
-        for (i = 0; i <= 8; i++) {
-            tabuleiro[i] = ' ';
-        }
+        atualizarPlacares();
+        sleep(1000);
 
-        atualizarBotoes();
-
-        primeiroJogador = trocarJogador(primeiroJogador)
-        jogadorAtual = primeiroJogador;
-
-        if (primeiroJogador === 'o') {
-            iaIniciar('o');
-        }
+        novaPartida();
     }
 }
 
@@ -117,11 +124,10 @@ function checarVencedorHipotetico(tabuleiro) {
 
 function atualizarBotoes() {
     for (let i = 0; i <= 8; i++) {
-        document.getElementById(i).value = tabuleiro[i];
+        document.getElementById(i).value = tabuleiroReal[i];
         /*console.log(document.getElementById(i));*/
     }
 }
-
 
 function trocarJogador(atual) {
     if (atual == 'x') {
@@ -142,7 +148,7 @@ function sleep(milliseconds) {
 
 function tabuleiroVazio() {
     for (i = 0; i <= 8; i++) {
-        if (tabuleiro[i] !== ' ') {
+        if (tabuleiroReal[i] !== ' ') {
             return false;
         }
     }
@@ -152,83 +158,78 @@ function tabuleiroVazio() {
 
 /* FUNÇÕES RELACIONADAS À IA */
 
-function darNota(jogadorHipotetico) {
-    let nota = 0;
-
-    for (let casa = 0; casa < 9; casa++) {
-        if (tabaux[casa] == ' ') {
-            tabaux[casa] = jogadorHipotetico;
-
-            if (checarVencedorHipotetico(tabaux) == jogadorHipotetico) {
-                nota += 1;
-            }
-            else if (checarVencedorHipotetico(tabaux) == trocarJogador(jogadorHipotetico)) {
-                nota -= 1;
-            }
-            else {
-                nota += (-1) * darNota(trocarJogador(jogadorHipotetico));
-            }
-
-            tabaux[casa] = ' ';
-        }
-    }
-
-    return (nota);
-}
-
 function iaIniciar(simboloMaquina) {
-    sleep(100);
-    console.log("IA iniciada...");
-
     if (tabuleiroVazio()) {
         registrarJogada(Math.round(Math.random() * (8 - 0)), simboloMaquina);
         return;
     }
 
-    let nota = null;
-    let notaMaior = -999999;
-    let retorno = -1;
+    sleep(100);
+    console.log("IA iniciada...");
 
-    tabaux = [...tabuleiro]
+    let maiorNota = -999999;
+    let casaEscolhida = -1;
 
-    for (let casa = 0; casa < 9; casa++) {
-        if (tabaux[casa] == ' ') {
-            nota = 0;
-            tabaux[casa] = simboloMaquina;
+    let tabuleiroAuxiliar = [...tabuleiroReal]
 
-            if (checarVencedorHipotetico(tabaux) == simboloMaquina) { // checa se jogar nessa casa se vence o jogo imediatamente
+    for (let casa = 0; casa <= 8; casa++) {
+        if (tabuleiroAuxiliar[casa] == ' ') {
+            let notaCasa = 0;
+            tabuleiroAuxiliar[casa] = simboloMaquina; // supoe a hipotese de jogar nesta casa
+
+            if (checarVencedorHipotetico(tabuleiroAuxiliar) === simboloMaquina) { // checa se ao jogar nessa casa vence-se o jogo imediatamente
                 registrarJogada(casa, simboloMaquina);
                 return;
             }
-            else {
-                nota = darNota(simboloMaquina); // chama a função recursiva para gerar uma nota para essa casa
-            }
 
-            if (nota > notaMaior) {
-                notaMaior = nota;
-                retorno = casa;
+            notaCasa = avaliarJogadaHipotetica(tabuleiroAuxiliar, simboloMaquina); // chama a função recursiva para gerar uma nota para essa casa
+
+            if (notaCasa > maiorNota) {
+                maiorNota = notaCasa;
+                casaEscolhida = casa;
             }
-            else if (nota === notaMaior) { // caso haja duas casas com a mesma nota...
+            else if (notaCasa === maiorNota) { // caso haja duas casas com a mesma nota...
                 if (Math.floor(Math.random() * 100) % 2 == 1) { // ...escolhe uma delas aleatoriamente
-                    retorno = casa;
+                    casaEscolhida = casa;
                 }
             }
 
-            tabaux[casa] = ' '; // esvazia a casa
+            tabuleiroAuxiliar[casa] = trocarJogador(simboloMaquina); // supoe a hipotese de o jogador jogar nessa casa em seguida
 
-            tabaux[casa] = trocarJogador(simboloMaquina);
-
-            if (checarVencedorHipotetico(tabaux) == trocarJogador(simboloMaquina)) { // verifica se o humano ganharia na próxima rodada
-                notaMaior = 999999; // prioridade máxima para impedir
-                retorno = casa; // não impedirá caso seja possível vencer na mesma jogada
+            if (checarVencedorHipotetico(tabuleiroAuxiliar) === trocarJogador(simboloMaquina)) { // verifica se o humano ganharia na próxima rodada
+                maiorNota = 999999; // prioridade máxima para impedir
+                casaEscolhida = casa; // só não impedirá caso seja possível vencer na mesma jogada
             }
 
-            tabaux[casa] = ' '; // esvazia a casa para checar a próxima
+            tabuleiroAuxiliar[casa] = ' '; // esvazia a casa para checar a próxima
         }
     }
 
     /*console.log("IA: " + retorno);*/
 
-    registrarJogada(retorno, simboloMaquina);
+    registrarJogada(casaEscolhida, simboloMaquina);
 }
 
+function avaliarJogadaHipotetica(tabuleiroHipotetico, jogadorHipotetico) {
+    let nota = 0;
+
+    for (let casa = 0; casa <= 8; casa++) {
+        if (tabuleiroHipotetico[casa] === ' ') {
+            tabuleiroHipotetico[casa] = jogadorHipotetico;
+
+            if (checarVencedorHipotetico(tabuleiroHipotetico) === jogadorHipotetico) {
+                nota += 1;
+            }
+            else if (checarVencedorHipotetico(tabuleiroHipotetico) === trocarJogador(jogadorHipotetico)) {
+                nota -= 1;
+            }
+            else {
+                nota += (-1) * avaliarJogadaHipotetica(trocarJogador(jogadorHipotetico));
+            }
+
+            tabuleiroHipotetico[casa] = ' ';
+        }
+    }
+
+    return (nota);
+}
